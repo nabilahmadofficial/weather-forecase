c74d679ea90c0d0ddf168db2dcf88870443dd4ce
Nabil <nabilahmad3034@gmail.com>
2023-09-27 16:18:57 +0500

    added new output data

--- script.js
+++ script.js
@@ -1,75 +1,138 @@
-// OpenWeatherMap API Key
+// Config
 const API_KEY = '2e6cb081339bf569d3644bd9da544ddf';
 
-// Get location on page load
-navigator.geolocation.getCurrentPosition(function(position) {
+// Cached location
+let lat = localStorage.getItem('lat');
+let lon = localStorage.getItem('lon');
 
-  let lat = position.coords.latitude;
-  let lon = position.coords.longitude;
-
-  // Call API with location
+// Page load
+if (lat && lon) {
   getWeatherByLocation(lat, lon);
+} else {
+  getLocation();
+}
 
-});
-
-
-// Listen for search form submit
-document.querySelector('form').addEventListener('submit', function(e){
-  
-  // Prevent default form submit
+// Listen for search
+document.getElementById('weather-form').addEventListener('submit', e => {
   e.preventDefault();
 
-  // Get input value
   let city = document.getElementById('search-input').value;
-
-  // Get weather for city
   getWeatherByCity(city);
-
 });
 
+// Get location
+function getLocation() {
+  navigator.geolocation.getCurrentPosition(pos => {
+    let lat = pos.coords.latitude;
+    let lon = pos.coords.longitude;
+
+    localStorage.setItem('lat', lat);
+    localStorage.setItem('lon', lon);
+
+    getWeatherByLocation(lat, lon);
+  });
+}
 
 // Get weather by city name
 async function getWeatherByCity(city) {
-
-  // API weather url
   let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${API_KEY}`;
-
-  // Fetch data from API
-  let data = await fetch(url)
-                    .then(response => response.json())
-                    .catch(err => console.error(err));
-
-  // Display weather data
-  displayWeather(data);
-
+  try {
+    let response = await fetch(url);
+    if (response.ok) {
+      let data = await response.json();
+      displayWeather(data);
+    } else {
+      console.error('Error fetching weather data:', response.statusText);
+    }
+  } catch (error) {
+    console.error('Error fetching weather data:', error);
+  }
 }
 
-// Get weather by coordinates
+// Get weather by location
 async function getWeatherByLocation(lat, lon) {
-
-  // API weather url
   let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${API_KEY}`;
-
-  // Fetch data from API
-  let data = await fetch(url)
-                    .then(response => response.json())
-                    .catch(err => console.error(err));
-
-  // Display weather data                    
-  displayWeather(data);
-
+  try {
+    let response = await fetch(url);
+    if (response.ok) {
+      let data = await response.json();
+      displayWeather(data);
+    } else {
+      console.error('Error fetching weather data:', response.statusText);
+    }
+  } catch (error) {
+    console.error('Error fetching weather data:', error);
+  }
 }
 
-// Display weather data in UI
+// Display weather
 function displayWeather(data) {
-  let html = `
-    <h2>${data.name} <img src="https://openweathermap.org/img/w/${data.weather[0].icon}.png" alt="${data.weather[0].description}"></h2>
-    <p>${data.weather[0].description}</p>
-    <p>Temperature: ${data.main.temp} &deg;C</p>
-    <p>Feels like: ${data.main.feels_like} &deg;C</p>
-    <p>Humidity: ${data.main.humidity}%</p>
-    <p>Wind speed: ${data.wind.speed} m/s</p>
+  const weatherData = document.getElementById('weather-data');
+  weatherData.innerHTML = ''; // Clear previous data
+
+  const cityName = data.name;
+  const temperature = data.main.temp;
+  const description = data.weather[0].description;
+  const icon = data.weather[0].icon;
+
+  const weatherHTML = `
+  <h3>${data.name} <img src="https://openweathermap.org/img/w/${data.weather[0].icon}.png" alt="${data.weather[0].description}"></h3>
+  <p><h4>${data.weather[0].description}</h4></p>
+  <p><b>Temperature: </b>${data.main.temp} &deg;C</p>
+  <p><b>Feels like: </b>${data.main.feels_like} &deg;C</p>
+  <p><b>Humidity: </b>${data.main.humidity}%</p>
+  <p><b>Wind speed:</b> ${data.wind.speed} m/s</p>
   `;
 
-  document.getElementById('weather-data').innerHTML = html;
+  weatherData.innerHTML = weatherHTML;
 }
+
+// Autocomplete
+const input = document.getElementById('search-input');
+const suggestionsList = document.getElementById('suggestions-list');
+
+input.addEventListener('input', async e => {
+  const query = e.target.value;
+  const url = `https://api.openweathermap.org/data/2.5/find?q=${query}&type=like&sort=population&cnt=5&appid=${API_KEY}`;
+
+  suggestionsList.innerHTML = '';
+
+  if (query.trim() !== '') {
+    try {
+      let response = await fetch(url);
+      if (response.ok) {
+        let data = await response.json();
+        if (data.list && data.list.length > 0) {
+          data.list.forEach(city => {
+            const suggestionItem = document.createElement('li');
+            suggestionItem.textContent = city.name;
+            suggestionItem.addEventListener('click', () => {
+              input.value = city.name;
+              suggestionsList.innerHTML = '';
+              getWeatherByCity(city.name);
+            });
+
+            suggestionsList.appendChild(suggestionItem);
+          });
+
+          suggestionsList.style.display = 'block';
+        } else {
+          suggestionsList.style.display = 'none';
+        }
+      } else {
+        console.error('Error fetching suggestions:', response.statusText);
+      }
+    } catch (error) {
+      console.error('Error fetching suggestions:', error);
+    }
+  } else {
+    suggestionsList.style.display = 'none';
+  }
+});
+
+// Hide suggestions when clicking outside the input field
+document.addEventListener('click', e => {
+  if (e.target !== input) {
+    suggestionsList.style.display = 'none';
+  }
+});
